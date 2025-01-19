6c2bf8c03e3f165c6ede10407957470a0a5b3a7b
akugizi <peterakugizi@gmail.com>
2025-01-19 11:28:00 +0300

    Add files via upload

---
+++ ISBN.java
@@ -0,0 +1,21 @@
+
+public class ISBN {
+ 
+    private String isbn;
+
+    public ISBN(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void getIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+}
+
+  
+    
+
